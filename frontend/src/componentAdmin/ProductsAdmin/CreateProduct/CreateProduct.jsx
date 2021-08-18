4b8b3f0b1922a530d52280118d71e0abe8d22e9b
Huu Huy <s3817719@rmit.edu.vn>
2021-08-18 19:54:51 +0700

    change UI

--- frontend/src/componentAdmin/ProductsAdmin/CreateProduct/CreateProduct.jsx
+++ frontend/src/componentAdmin/ProductsAdmin/CreateProduct/CreateProduct.jsx
@@ -1,12 +1,14 @@
-import React, { useState } from 'react'
-import { TextField, Button, Typography, Paper, Box, Grid, CircularProgress } from '@material-ui/core'
-import FileBase from 'react-file-base64'
-import { useDispatch, useSelector } from 'react-redux';
+import { Button, CircularProgress, Container, CssBaseline, TextField, Typography } from '@material-ui/core';
 import Alert from '@material-ui/lab/Alert';
+import React, { useState } from 'react';
+import FileBase from 'react-file-base64';
+import { useDispatch, useSelector } from 'react-redux';
 import { createdProduct } from '../../../actions/productActions';
+import useStyles from './styles';
 
 const CreateProduct = () => {
     const dispatch = useDispatch()
+    const classes = useStyles()
     const [productData, setProductData] = useState({
         name: '', image: '', brand: '', category: '', description: '', price: null, rating: null, numReviews: null
     });
@@ -17,41 +19,43 @@ const CreateProduct = () => {
         dispatch(createdProduct(productData))
     }
     return (
-        <Box mt={3}>
 
 
-            <Box ml={6} mr={6}>
+        <Container component="main" maxWidth="xs">
+            <CssBaseline />
+            <div className={classes.paper}>
 
-                <Grid container spacing={5}>
-                    <Grid item xs={12}>
-                        <Box marginBottom={3}>
-                            <Paper elevation={3}>
-                                <form style={{ padding: "20px" }} autoComplete="off" noValidate onSubmit={handleSubmit}>
-                                    <Typography variant="h6">
-                                        Create Product
-                                    </Typography>
-                                    {loading && <CircularProgress color="secondary" />}
-                                    {error && <Alert severity="error">{error}</Alert>}
-                                    {success && <Typography>Success</Typography>}
-                                    <TextField name="name" variant="outlined" label="Name" fullWidth value={productData.name} onChange={(e) => setProductData({ ...productData, name: e.target.value })}></TextField>
-                                    <div>
-                                        <FileBase type="file" multiple={false} onDone={({ base64 }) => setProductData({ ...productData, image: base64 })}>
-                                        </FileBase>
-                                    </div>
-                                    <TextField name="brand" variant="outlined" label="Brand" fullWidth value={productData.brand} onChange={(e) => setProductData({ ...productData, brand: e.target.value })}></TextField>
-                                    <TextField name="category" variant="outlined" label="Category" fullWidth value={productData.category} onChange={(e) => setProductData({ ...productData, category: e.target.value })}></TextField>
-                                    <TextField name="description" variant="outlined" label="Description" fullWidth value={productData.description} onChange={(e) => setProductData({ ...productData, description: e.target.value })}></TextField>
-                                    <TextField name="price" type="Number" variant="outlined" label="Price" fullWidth value={productData.price} onChange={(e) => setProductData({ ...productData, price: +e.target.value })}></TextField>
-                                    <TextField name="rating" type="Number" variant="outlined" label="Rating" fullWidth value={productData.rating} onChange={(e) => setProductData({ ...productData, rating: +e.target.value })}></TextField>
-                                    <TextField name="numReviews" type="Number" variant="outlined" label="Num Reviews" fullWidth value={productData.numReviews} onChange={(e) => setProductData({ ...productData, numReviews: +e.target.value })}></TextField>
-                                    <Button variant="contained" color="primary" size="large" type="submit" fullWidth>Submit</Button>
-                                </form>
-                            </Paper>
-                        </Box>
-                    </Grid>
-                </Grid>
-            </Box>
-        </Box>
+                <Typography component="h1" variant="h5">
+                    CREATE NEW PRODUCT
+                </Typography>
+                {loading && <CircularProgress style={{ marginTop: '10px' }} color="secondary" />}
+                {error && <Alert style={{ marginTop: '10px' }} severity="error">{error}</Alert>}
+                {success && <Alert style={{ marginTop: '10px' }} severity="success" fullWidth>Success</Alert>}
+                <form className={classes.form} onSubmit={handleSubmit} noValidate>
+                    <TextField autoComplete="name" autoFocus margin="normal" name="name" variant="outlined" label="Name" fullWidth value={productData.name} onChange={(e) => setProductData({ ...productData, name: e.target.value })}></TextField>
+                    <TextField margin="normal" name="brand" variant="outlined" label="Brand" fullWidth value={productData.brand} onChange={(e) => setProductData({ ...productData, brand: e.target.value })}></TextField>
+                    <TextField margin="normal" name="category" variant="outlined" label="Category" fullWidth value={productData.category} onChange={(e) => setProductData({ ...productData, category: e.target.value })}></TextField>
+                    <TextField margin="normal" name="description" variant="outlined" label="Description" fullWidth value={productData.description} onChange={(e) => setProductData({ ...productData, description: e.target.value })}></TextField>
+                    <TextField margin="normal" name="price" type="Number" variant="outlined" label="Price" fullWidth value={productData.price} onChange={(e) => setProductData({ ...productData, price: +e.target.value })}></TextField>
+                    <TextField margin="normal" name="rating" type="Number" variant="outlined" label="Rating" fullWidth value={productData.rating} onChange={(e) => setProductData({ ...productData, rating: +e.target.value })}></TextField>
+                    <TextField margin="normal" name="numReviews" type="Number" variant="outlined" label="Num Reviews" fullWidth value={productData.numReviews} onChange={(e) => setProductData({ ...productData, numReviews: +e.target.value })}></TextField>
+                    <div>
+                        <FileBase type="file" multiple={false} onDone={({ base64 }) => setProductData({ ...productData, image: base64 })}>
+                        </FileBase>
+                    </div>
+                    <Button style={{ backgroundColor: '#f73471' }}
+                        type="submit"
+                        fullWidth
+                        variant="contained"
+                        color="primary"
+                        className={classes.submit}
+                    >
+                        Create
+                    </Button>
+                </form>
+            </div>
+        </Container >
+
 
     )
 }
