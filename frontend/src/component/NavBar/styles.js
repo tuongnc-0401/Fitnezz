ec123fead6cb17c28e188ca7b70861f5803e0314
Huynguyen2709 <s3825059@rmit.edu.vn>
2021-08-28 20:55:31 +0700

    Finish updating new navbar

--- frontend/src/component/NavBar/styles.js
+++ frontend/src/component/NavBar/styles.js
@@ -4,8 +4,72 @@ export default makeStyles((theme) => ({
         backgroundColor: "black",
     },
     itemsWrapper: {
-        flexGrow: 1
+        flexGrow: 1,
+        padding: '0px 10%',
+        display: 'flex',
+        justifyContent: 'space-evenly',
+        [theme.breakpoints.down('1100')]: {
+            backgroundColor: "black",
+            width: '60%',
+            position: 'absolute',
+            left: '0px',
+            height: '95vh',
+            top: '6vh',
+            zIndex: '100',
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+            transform: 'translateX(-100%)',
+            transition: '0.5s ease-in-out',
+        },
     },
+
+    itemsWrapperActive: {
+        flexGrow: 1,
+        display: 'flex',
+        justifyContent: 'space-evenly',
+        [theme.breakpoints.down('1100')]: {
+            backgroundColor: "black",
+            width: '60%',
+            position: 'absolute',
+            left: '0px',
+            height: '95vh',
+            top: '6vh',
+            zIndex: '100',
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+            transform: 'translateX(0%)',
+            transition: '0.5s ease-in-out',
+        },
+
+        [theme.breakpoints.down('510')]: {
+            backgroundColor: "black",
+            width: '100%',
+            position: 'absolute',
+            left: '0px',
+            height: '95vh',
+            top: '6vh',
+            zIndex: '100',
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+            transform: 'translateX(0%)',
+            transition: '0.5s ease-in-out',
+        },
+
+
+    },
+
+    burger: {
+        display: 'none',
+        padding: '10px 15px',
+        cursor: 'pointer',
+        [theme.breakpoints.down('1100')]: {
+            display: 'block'
+        },
+    },
+
     root: {
         flexGrow: 1,
     },
@@ -16,7 +80,15 @@ export default makeStyles((theme) => ({
         alignItems: 'center',
         display: 'flex',
         textDecoration: 'none',
+        marginLeft: '20px'
     },
+
+    logoTitleNoPadding: {
+        alignItems: 'center',
+        display: 'flex',
+        textDecoration: 'none',
+    },
+
     image: {
         marginRight: '10px',
     },
@@ -24,7 +96,8 @@ export default makeStyles((theme) => ({
         marginRight: '10px'
     },
     admin: {
-        marginLeft: '10px'
+        marginLeft: '10px',
+        marginRight: '20px'
     },
     pink: {
         color: "white",
